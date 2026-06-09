a490eb6426215f4e152eafb3665b3c095723afe7
liya <you@example.com>
2026-06-09 17:47:59 +0530

    all files

--- server/src/model/user.model.js
+++ server/src/model/user.model.js
@@ -4,13 +4,13 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true, "Username is required"],
       unique: true,
       trim: true,
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
       lowercase: true,
       trim: true,
@@ -21,8 +21,8 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
-      minlength: 6,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
       select: false,
     },
     role: {
